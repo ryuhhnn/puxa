3f9c12c6f71811602fbbe039f9cd892aa63c4974
Ryan Coleman <me@ryuhhnn.com>
2025-05-14 02:29:40 -0400

    update model version and next version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import { Difficulty } from "@/lib/types";
 
 export const DB_FILENAME = "./data/puxa.db";
-export const LLM_MODEL = "llama3.1";
+export const LLM_MODEL = "llama3.2";
 
 export const DEFAULT_DIFFICULTY = Difficulty.BASIC;
